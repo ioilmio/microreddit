dda01d0d82bef5e67009c1ce710b3b95e232c755
ioilmio <illuminato.salvatore@gmail.com>
2020-07-14 00:46:10 +0200

    edit view and controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-  @user = User.new
+    @user = User.new
   end
 
   def create
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if user.save
+      redirect_to users_path
+    else
+      render :edit
     end
   end
 
