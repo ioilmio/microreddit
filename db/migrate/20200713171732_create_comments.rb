4a16049921e147118ac2ce8f533c9a5d02fd5ba8
ioilmio <illuminato.salvatore@gmail.com>
2020-07-15 18:51:07 +0200

    user instead of user_id in model associations

--- db/migrate/20200713171732_create_comments.rb
+++ db/migrate/20200713171732_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :user_id, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :body
 
       t.timestamps
