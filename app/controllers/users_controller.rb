ece5018f06114847cd9921eeb2ba7c5dc239101c
ioilmio <illuminato.salvatore@gmail.com>
2020-07-15 20:03:33 +0200

    add reference post to comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,13 +26,31 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to edit_user_path
     else
-      redirect_to user_path
+      render :edit
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.save
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users
+
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
 end
